1b43cae2bb1989d4cc4a9fe9c9be21207cb6495f
javiermt98 <javiermtdam@gmail.com>
2021-02-24 11:45:53 +0100

    Ejercicio 5 - a

--- pf_musica/src/main/java/ejercicios/Ejercicio04_MongoDB.java
+++ pf_musica/src/main/java/ejercicios/Ejercicio04_MongoDB.java
@@ -23,7 +23,7 @@ public class Ejercicio04_MongoDB {
 	public static void main(String[]args) {
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost/27017");
 		MongoDatabase database = mongoClient.getDatabase("musica");
-		MongoCollection<Document> collection = database.getCollection("grupo");
+		MongoCollection<Document> collectiongrupo = database.getCollection("grupo");
 		MongoCollection<Document> collectioncompany = database.getCollection("company");
 		
 
@@ -36,7 +36,7 @@ public class Ejercicio04_MongoDB {
 		System.out.println("Pais: ");
 		String pais = p.nextLine();
 		List<Document> documentos = new ArrayList<Document>();
-		collection.find(regex("pais", pais)).into(documentos);
+		collectiongrupo.find(regex("pais", pais)).into(documentos);
 		System.out.println("Grupos de "+pais+": ");
 		for(Document grupo: documentos) {
 			System.out.println(grupo.getString("nombre_grupo")+" - "+grupo.getInteger("cod_grupo"));
@@ -48,10 +48,10 @@ public class Ejercicio04_MongoDB {
 		//***************************************************************************************
 		System.out.println("\n******************* Apartado B *******************");
 		Scanner a = new Scanner(System.in);
-		System.out.println("Pais: ");
+		System.out.print("Grupo: ");
 		String gr = a.nextLine();
 		documentos = new ArrayList<Document>();
-		collection.find().into(documentos);
+		collectiongrupo.find(regex("nombre_grupo",gr)).into(documentos);
 		System.out.println("Canciones de "+gr+": ");
 		for(Document grupo: documentos) {
 			
@@ -60,7 +60,7 @@ public class Ejercicio04_MongoDB {
 				List<Document> listacanciones = (List<Document>) disco.get("canciones");
 				
 				for(Document cancion: listacanciones) {
-					
+					System.out.println(cancion.get("titulo_cancion")+": "+cancion.get("duracion")+"min");
 				}
 			}
 			
@@ -75,7 +75,26 @@ public class Ejercicio04_MongoDB {
 		
 		System.out.println("\n******************* Apartado C *******************");
 
+		Scanner c = new Scanner(System.in);
+		System.out.println("Companyia: ");
+		String com = a.nextLine();
+		documentos = new ArrayList<Document>();
+		collectioncompany.find(regex("nombre_company",com)).into(documentos);
 		
+		List<Document>grupos = new ArrayList<Document>();
+		collectiongrupo.find().into(grupos);
+		int cod_comp=-1;
+		for(Document companyia: documentos) {
+			cod_comp = companyia.getInteger("cod_company");
+		}
+		for(Document grupo: grupos) {
+			List<Document> discos = (List<Document>) grupo.get("disco");
+			for(Document disc: discos) {
+				if(disc.getInteger("cod_company") == cod_comp) {
+					System.out.println(disc.get("nombre_disco")+": "+disc.get("fecha_disco"));
+				}
+			}
+		}
 		
 		
 		
@@ -83,7 +102,23 @@ public class Ejercicio04_MongoDB {
 		//APARTADO D - MOSTRAR TITULO, DURACION Y DISCO (NOMBRE Y FECHA DE LANZAMIENTO) DE LAS CANCIONES DE MAS DE 4MIN
 		//**************************************************************************************************************
 		System.out.println("\n******************* Apartado D *******************");
-
+		documentos = new ArrayList<Document>();
+		collectiongrupo.find().into(documentos);
+		System.out.println("Canciones de mas de 4 min: ");
+		for(Document grupo: documentos) {
+			
+			List<Document> listadiscos = (List<Document>) grupo.get("disco");
+			for(Document disco:listadiscos) {
+				List<Document> listacanciones = (List<Document>) disco.get("canciones");
+				
+				for(Document cancion: listacanciones) {
+					if(cancion.getDouble("duracion")>4) {
+					System.out.println(cancion.get("titulo_cancion")+": "+cancion.get("duracion")+"min");
+					}
+				}
+			}
+			
+		}
 		
 		
 		
@@ -93,8 +128,26 @@ public class Ejercicio04_MongoDB {
 		//APARTADO E - MOSTRAR DE CADA GRUPO, SU NOMBRE, EL DE SUS DISCOS Y LA COMPANYIA DONDE PUBLICARON CADA DISCO
 		//**************************************************************************************************************
 		System.out.println("\n******************* Apartado E *******************");
-
+		documentos = new ArrayList<Document>();
+		collectiongrupo.find().into(documentos);
 		
+		List<Document> companyias = new ArrayList<Document>();
+		collectioncompany.find().into(companyias);
+		
+		for(Document grupo: documentos) {
+			System.out.println("\nGrupo: "+grupo.get("nombre_grupo"));
+			List<Document> listadiscos = (List<Document>) grupo.get("disco");
+			for(Document disco:listadiscos) {
+				System.out.println("Disco: "+disco.get("nombre_disco"));
+				for(Document comp: companyias) {
+					if(disco.getInteger("cod_company") == comp.getInteger("cod_company")) {
+						System.out.println("Companyia: "+comp.get("nombre_company"));
+					}
+				}
+				
+			}
+			
+		}
 		
 		
 		
